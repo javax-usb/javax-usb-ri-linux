33333404dc82b57a07b7c7fd78b3eca38fb2c9ff
Dan Streetman <ddstreet@us.ibm.com>
2002-08-09 16:35:25 +0000

    changed name

---
+++ src/com/ibm/jusb/os/linux/LinuxDeviceOsImp.java
@@ -0,0 +1,56 @@
+package com.ibm.jusb.os.linux;
+
+/*
+ * Copyright (c) 1999 - 2001, International Business Machines Corporation.
+ * All Rights Reserved.
+ *
+ * This software is provided and licensed under the terms and conditions
+ * of the Common Public License:
+ * http://oss.software.ibm.com/developerworks/opensource/license-cpl.html
+ */
+
+import javax.usb.*;
+
+import com.ibm.jusb.*;
+import com.ibm.jusb.os.*;
+
+/**
+ * UsbDeviceOsImp implemenation for Linux platform.
+ * <p>
+ * This must be set up before use.
+ * <ul>
+ * <li>The {@link #getUsbDeviceImp() UsbDeviceImp} must be set
+ *     either in the constructor or by its {@link #setUsbDeviceImp(UsbDeviceImp) setter}.</li>
+ * <li>The {@link #getLinuxDeviceProxy() LinuxDeviceProxy} must be set
+ *     either in the constructor or by its {@link #setLinuxDeviceProxy(LinuxDeviceProxy) setter}.</li>
+ * </ul>
+ * @author Dan Streetman
+ */
+class LinuxDeviceOsImp implements UsbDeviceOsImp
+{
+	/** Constructor */
+	public LinuxDeviceOsImp( UsbDeviceImp device, LinuxDeviceProxy proxy )
+	{
+		setUsbDeviceImp(device);
+		setLinuxDeviceProxy(proxy);
+	}
+
+	/** @return The UsbDeviceImp for this */
+	public UsbDeviceImp getUsbDeviceImp() { return usbDeviceImp; }
+
+	/** @param device The UsbDeviceImp for this */
+	public void setUsbDeviceImp( UsbDeviceImp device ) { usbDeviceImp = device; }
+
+	/** @return The LinuxDeviceProxy for this */
+	public LinuxDeviceProxy getLinuxDeviceProxy() { return linuxDeviceProxy; }
+
+	/** @param proxy The LinuxDeviceProxy */
+	public void setLinuxDeviceProxy(LinuxDeviceProxy proxy) { linuxDeviceProxy = proxy; }
+
+	//**************************************************************************
+	// Instance variables
+
+	private UsbDeviceImp usbDeviceImp = null;
+
+	private LinuxDeviceProxy linuxDeviceProxy = null;
+}
