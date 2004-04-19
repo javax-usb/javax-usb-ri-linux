def5d7e847adcba6c42a903cd4d1fe04d5c89cf6
Dan Streetman <ddstreet@us.ibm.com>
2004-04-19 16:17:04 +0000

    updated version to 0.10.5

--- src/com/ibm/jusb/os/linux/Version.java
+++ src/com/ibm/jusb/os/linux/Version.java
@@ -24,7 +24,7 @@ public class Version
 	}
 
 	public static final String LINUX_API_VERSION = "0.10.1";
-	public static final String LINUX_IMP_VERSION = "0.10.5-CVS";
+	public static final String LINUX_IMP_VERSION = "0.10.5";
 	public static final String LINUX_IMP_DESCRIPTION =
 		 "JSR80 : javax.usb"
 		+"\n"
