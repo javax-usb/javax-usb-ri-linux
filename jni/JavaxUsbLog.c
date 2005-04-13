a509185a8a12d80a134ed84380565fedc2b9eb70
Dan Streetman <ddstreet@us.ibm.com>
2005-04-13 18:33:07 +0000

    fix misnamed JNI function name

--- jni/JavaxUsbLog.c
+++ jni/JavaxUsbLog.c
@@ -21,7 +21,7 @@ jboolean trace_xfer = JNI_TRUE;
 jboolean trace_urb = JNI_FALSE;
 int trace_level = LOG_CRITICAL;
 
-JNIEXPORT void JNICALL Java_com_ibm_jusb_os_linux_JavaxUsb_nativeSetTraceData
+JNIEXPORT void JNICALL Java_com_ibm_jusb_os_linux_JavaxUsb_nativeSetTracing
 (JNIEnv *env, jclass JavaxUsb, jboolean enable)
 {
 	tracing = enable;
