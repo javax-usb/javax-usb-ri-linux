559ad757a873901eb9c992b8afda6029b8a7dee2
Dan Streetman <ddstreet@us.ibm.com>
2008-06-03 19:49:24 +0000

    Update version number to 1.0.2 actual

--- src/com/ibm/jusb/os/linux/Version.java
+++ src/com/ibm/jusb/os/linux/Version.java
@@ -24,7 +24,7 @@ public class Version
 	}
 
 	public static final String LINUX_API_VERSION = "1.0.0";
-	public static final String LINUX_IMP_VERSION = "1.0.2-CVS";
+	public static final String LINUX_IMP_VERSION = "1.0.2";
 	public static final String LINUX_IMP_DESCRIPTION =
 		 "JSR80 : javax.usb"
 		+"\n"
